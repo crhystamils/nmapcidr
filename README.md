ffae00222487058bb2dabead8f867473e04cd975
Cristhian Lima S <crhystamil@gmail.com>
2019-11-20 14:40:41 -0500

    [ new options added ]

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # nmapcidr
-Used Nmap For CIDR Network notation
+Used Nmap For list IP or CIDR Network notation
 
-For use:
+## USAGE:
 
-*first:*
-fill the file lista.txt with CIDR networs one per line.
+help:
+```bash
+$ ./nmap_cidr.sh
+```
 
-e.g lista.txt
-127.0.0.1/30
-10.0.0.1/31
+scan ip list:
+```bash
+$ ./nmap_cidr.sh -iL ip_list
+```
+scan CIDR list: 
+```bash
+$ ./nmap_cidr.sh -rL range_list
+```
 
-*second:*
-execute the script.
-> sh nmap_cidr.sh
+to enable more processes, default is 10:
+```bash
+$ ./nmap_cidr.sh -ir/-rl <file> -t <Number Process>
+$ ./nmap_cidr.sh -ir/-rl file_list -t 30
+```
+
+add new options to nmap, default is -A -Pn:
+```bash
+$ ./nmap_cidr.sh -ir/-rl file_list -t 30 "<options nmap>"
+$ ./nmap_cidr.sh -ir/-rl file_list -t 30 "-sC -sT -p 8080" 
+```
 
 The script make a directory for CIDR network and save the xml format nmap output in one file per host of the CIDR network in this directory.
--- nmap_cidr.sh
+++ nmap_cidr.sh
@@ -1,20 +1,122 @@
 #!/bin/bash
-for line in $(cat lista.txt); do 
-	echo "Se esta Procesando El rango de Red $line" ;
-	carpeta=$(echo $line  | sed 's/\//_barra_/g' ):
-	if [ ! -d "$carpeta" ]; then
-		mkdir $carpeta;
-		echo "Se creo el directorio $carpeta";
-	else
-		echo "El directorio  $carpeta ya existe";
 
-	fi
-	declare -a ips=$(./cidr.py $line);
-#	echo $ips;
-	for i in $ips
-	do
-		echo "Se empieza con el scaneo del equipo $i se guardara en la carpeta $carpeta";
-		nmap -A -p- -P0 -vv $i -oX ./$carpeta/$i.xml
-	done
+declare options=" -A -Pn "
+declare filename=""
+declare threads=10
+
+function nmap_cidr {
+for line in $(cat $filename); do 
+    echo "[*] Se esta Procesando El rango de Red $line" ;
+    carpeta=$(echo $line  | sed 's/\//_/g' ):
+    if [ ! -d "$carpeta" ]; then
+        mkdir ./scan/$carpeta;
+#    else
+#        echo "El directorio  $carpeta ya existe";
+    fi
+    
+    declare -a ips=$(./cidr.py $line);
+    numthreads=$(ps -def |grep nmap | wc -l);
+
+    for ip in $ips
+    do
+        while [ true ]; do
+            numthreads=$(ps -def |grep nmap | wc -l);
+            if [ $numthreads -le $threads ]; then
+                echo "[*] Init scan $ip in ./scan/$carpeta/$ip.xml {$numthreads}" 
+                nmap $options $ip -oA ./scan/$carpeta/$ip > /dev/null &
+#                nmap $options $ip -oX ./scan/$carpeta/$ip.xml > /dev/null &
+#                xsltproc ./$carpeta/$i.xml -o ./$carpeta/$i.html&
+                sleep 2;
+                break;
+            else
+                sleep 5;
+	    fi
+        done
+
+    done
 done
+   
+}
+
+function nmap_ipList {
+    for ip in $(cat $filename); do
+        while [ true ];do
+            num_threads=$(ps -def | grep nmap | wc -l);
+            if [ $num_threads -le $threads ]; then
+                echo "[*] Init scan $ip in ./scan/$ip.xml {$num_threads}" 
+                nmap $options $ip -oA ./scan/$ip > /dev/null &
+#                nmap $options $ip -oX ./scan/$ip.xml > /dev/null &
+#                xsltproc ./$carpeta/$i.xml -o ./$carpeta/$i.html&
+                sleep 2;
+                break;
+            else
+                sleep 5
+            fi
+        done
+    done
+}
+
+
+if [ -z $1 ]; then
+    echo "[*] Parallel scan nmap"
+    echo "[*] Usage: $0 < -iL list IPs/ -rL list Range ip >  < -t NumberTreads > \"<options nmap>\"" 
+    exit
+fi
+
+if [ -f $2 ]; then
+    filename="$2"
+    echo "[*] Initial scanning to file $2"
+else
+    echo "[*] File not found $2, change file."
+    exit
+fi
+
+if [ ! -d 'scan' ]; then
+    mkdir scan;
+    echo "[*] created directory"
+fi 
+
+if [ -z "$5" ]; then
+    echo "[*] use default options nmap -A -Pn"
+else
+    options=$5
+    echo "[*] use nmap options: nmap $options X.X.X.X -oX x.x.x.x.xml"
+fi
+
+if [ -z "$3" ]; then
+
+    echo "[*] default number process is 10 "
+    case "$1" in
+        -iL)
+            echo "[*] scan IP list"
+            nmap_ipList
+            ;;
+        -rL)
+            echo "[*] scan CIDR list"
+            nmap_cidr
+            ;;
+        *)
+            echo "Execute: ./nmap_cidr.sh"
+            exit 1
+    esac
+
+else
+    echo [*] number process is $4
+    threads=$4
+    case "$1" in
+        -iL)
+            echo "[*] scan lista of IP"
+            nmap_ipList
+            ;;
+        -rL)
+            echo "[*] scan range of IP"
+            nmap_cidr
+            ;;
+        *)
+            echo "Execute: ./nmap_cidr.sh"
+            exit 1
+    esac
+
+fi
+
 
